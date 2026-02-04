d3e24a12c4c54c0538ac48c2f272916be122da56
Edwing76 <mingolo3arc@gmail.com>
2026-02-05 00:43:33 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'superlist-v1';
+const assets = [
+  'index.html',
+  'crear.html',
+  'lista.html',
+  'estilos.css'
+];
+
+// Instalar y guardar archivos en caché
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Responder desde la caché cuando no hay internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
